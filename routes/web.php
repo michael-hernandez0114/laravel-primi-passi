7047dccee75786f2c5c174625b81da7b23d1e9e3
Michael Hernandez <gammabeta010@gmail.com>
2020-05-04 23:29:02 +0200

    Primi passi usando blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('users.index');
 });
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home');
