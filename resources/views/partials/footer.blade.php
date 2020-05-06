497b9b982d3c3d2f3ad56522cd9ab91be8f798d4
Michael Hernandez <gammabeta010@gmail.com>
2020-05-06 22:28:30 +0200

    Finito sito LG.

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,62 @@
+<footer>
+    <div class="footer-blocks container">
+        <section id="left">
+            <div class="footer-img">
+                <img src="{{asset('img/marchio-sito-test.png')}}" alt="">
+            </div>
+
+            <ul>
+                <li>Ragione sociale: La Molisana S.P.A</li>
+                <li>Sede legale: Contrada Colle delle Api, 100/A</li>
+                <li>86100 - Campobasso (CB)</li>
+                <li>Pec: lamolisana@pec.it</li>
+                <li>Tel: +39 0874 4981</li>
+                <li>Fax: +39 0874 629584</li>
+                <li>info@lamolisana.it (per segnalazioni degil utenti)</li>
+                <li>commerciale@lamolisana.it</li>
+                <li>export@lamolisana.it</li>
+                <li>numero verde 800818081</li>
+                <li>telefono 3801292455</li>
+            </ul>
+        </section>
+        <section id="middle">
+            <h2>pastificio</h2>
+            <ul class="spaced-list">
+                <li><a href="#">Il Pastificio</a> </li>
+                <li><a href="#">Grano decorticato a pietra</a> </li>
+                <li><a href="#">Il Molisce c'&eacute;</a> </li>
+                <li><a href="#">Filiera Integrata</a> </li>
+                <li><a href="#">100 anni di pasta</a> </li>
+                <li><a href="#">Sartoria della pasta</a> </li>
+                <li><a href="#">Spaghetto Quadrato</a> </li>
+                <li><a href="#">Le Persone</a> </li>
+            </ul>
+            <h2>prodotti</h2>
+            <ul class="spaced-list">
+                <li><a href="#">Le Classiche</a> </li>
+                <li><a href="#">Le Integrali</a> </li>
+                <li><a href="#">Le Speciali</a> </li>
+                <li><a href="#">Le Biologiche</a> </li>
+                <li><a href="#">Le Gluten-Free</a> </li>
+                <li><a href="#">Le Semole</a> </li>
+                <li><a href="#">Le Extra di Lusso</a> </li>
+            </ul>
+        </section>
+        <section id="right">
+            <h2>Collezione da Chef</h2>
+            <ul class="spaced-list">
+                <li><a href="#">Collezione da Chef</a> </li>
+                <li><a href="#">Grandi Cucine</a> </li>
+                <li><a href="#">Biologiche</a> </li>
+                <li><a href="#">Quadrate</a> </li>
+            </ul>
+        </section>
+
+    </div>
+
+    <div class="bottom-img">
+        <img src="{{asset('img/footer-montagne-bottom.jpg')}}" alt="">
+    </div>
+
+
+</footer>
