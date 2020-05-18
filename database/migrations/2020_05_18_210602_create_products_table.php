11360f6061fd4ebea0bf7aa887c0c317714cd998
Michael Hernandez <gammabeta010@gmail.com>
2020-05-18 23:30:05 +0200

    WIP laraval_molisana DB

---
+++ database/migrations/2020_05_18_210602_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->text('src');
+            $table->text('src-h');
+            $table->text('src-p');
+            $table->string('title', 100);
+            $table->string('type', 20);
+            $table->integer('cooking_time');
+            $table->integer('weight');
+            $table->longtext('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
